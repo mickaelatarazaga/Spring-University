5b6b839dfaea215f057ebd575d9e2a6cb40857a9
mickaelatarazaga <94411297+mickaelatarazaga@users.noreply.github.com>
2022-02-07 10:53:50 -0300

    Projects worked on intro

---
+++ Spring MVC y Thymeleaf/2.1 03-Ejercicio-CompartiendoInformacionModel/HolaMundoThymeleaf/src/main/java/mx/com/gm/ControladorInicio.java
@@ -0,0 +1,24 @@
+package mx.com.gm;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+public class ControladorInicio {
+    
+    @Value("${index.saludo}")
+    private String saludo;
+    
+    @GetMapping("/")
+    public String inicio(Model model){
+        var mensaje = "Mensaje con Thymeleaf";
+        log.info("ejecutando el controlador Spring MVC");
+        model.addAttribute("mensaje", mensaje);
+        model.addAttribute("saludo", saludo);
+        return "index";
+    }
+}
---
+++ Spring MVC y Thymeleaf/3.1 04-Ejercicio-PersonaDomain/HolaMundoThymeleaf/src/main/java/mx/com/gm/ControladorInicio.java
@@ -0,0 +1,33 @@
+package mx.com.gm;
+
+import lombok.extern.slf4j.Slf4j;
+import mx.com.gm.domain.Persona;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+public class ControladorInicio {
+    
+    @Value("${index.saludo}")
+    private String saludo;
+    
+    @GetMapping("/")
+    public String inicio(Model model){
+        var mensaje = "Mensaje con Thymeleaf";
+        
+        var persona = new Persona();
+        persona.setNombre("Juan");
+        persona.setApellido("Perez");
+        persona.setEmail("jperez@mail.com");
+        persona.setTelefono("55443322");
+        
+        log.info("ejecutando el controlador Spring MVC");
+        model.addAttribute("mensaje", mensaje);
+        model.addAttribute("saludo", saludo);
+        model.addAttribute("persona", persona);
+        return "index";
+    }
+}
